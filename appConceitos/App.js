ce35c639be38d8da29dabf7d3fba1de3a9a583e9
Daniele Costa <54379940+costadaniele@users.noreply.github.com>
2022-03-14 16:23:52 -0300

    Aula 04

--- appConceitos/App.js
+++ appConceitos/App.js
@@ -1,19 +1,3 @@
-/*import React, { Component } from 'react';
-import { View, Text} from 'react-native';
- 
-class App extends Component{
-  render(){
-    return(
-      <View>
-        <Text>Olá Turma novamente!</Text>
-        <Text>Meu Primeiro App</Text>
-      </View>
-    )
-  }
-}
- 
-export default App;*/
-
 import React from 'react';
 import { View, Text, Image} from 'react-native';
 
@@ -36,3 +20,46 @@ function App(){
 }
  
 export default App;
+
+/*import React, { Component } from 'react';
+import { View, Text, Image } from 'react-native';
+class App extends Component {
+  render() {
+    let nome = 'Meu Primeiro App';
+    let img =
+      'https://vejasp.abril.com.br/wp-content/uploads/2016/12/ads_macgyver1.jpg';
+    return (
+      <View>
+        <Text>Olá Turma novamente!</Text>
+        <Text>Meu Primeiro App</Text>
+        <Text style={{ color: '#FF0000', fontSize: 25, margin: 15 }}>
+          React Native
+
+        </Text>
+        <Image
+          source={{ uri: img }}
+          style={{ width: 300, height: 300 }}
+        />
+        <Text style={{ fontSize: 30 }}>{nome}</Text>
+      </View>
+    )
+  }
+}
+export default App;*/
+
+/*import React, { Component } from 'react';
+import { View, Text} from 'react-native';
+ 
+class App extends Component{
+  render(){
+    return(
+      <View>
+        <Text>Olá Turma novamente!</Text>
+        <Text>Meu Primeiro App</Text>
+      </View>
+    )
+  }
+}
+ 
+export default App;*/
+
